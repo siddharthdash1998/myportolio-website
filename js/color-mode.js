3ff858ce0527f6e3669b225678d180ca37eb78bb
Siddharth Dash <dash.sid@northeastern.edu>
2024-03-05 21:38:00 -0500

    Added the Base Page with Template for Light and Dark Mode

---
+++ js/color-mode.js
@@ -0,0 +1,26 @@
+// DEFINE DOM Elements 
+
+const toggleButton = document.querySelector("#toggle-button");
+const root = document.querySelector(":root");
+const storageKey =  "color-mode";
+const defaultMode = "light-mode";
+
+// Load the user's preferred color mode from local storage
+
+function loadColorMode() {
+    
+}
+
+// Toggle the color mode 
+toggleButton.addEventListener("click",()=>{
+
+});
+
+
+//Saving the user's preferred color mode to local storage 
+function saveColorMode() {
+    //Check if the root element has a class of dark-mode. if yes then the currewnt 
+    // mode is switched to light and vice versa 
+    
+}
+
