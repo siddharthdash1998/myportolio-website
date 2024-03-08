ab606108f4c7d9e517486a808bee3d5157ec4970
Siddharth Dash <dash.sid@northeastern.edu>
2024-03-08 16:42:20 -0500

    Added all features

--- js/color-mode.js
+++ js/color-mode.js
@@ -1,51 +1,39 @@
-// DEFINE DOM Elements 
-
+// Define DOM elements
 const toggleButton = document.querySelector("#toggle-button");
 const root = document.querySelector(":root");
-const storageKey =  "color-mode"; //Cache data on the browser
+const storageKey = "color-mode";
 const defaultMode = "light-mode";
 
-// Load the user's preferred color mode from local storage
-
+// Load the user's preffered color mode from local storage.
 function loadColorMode() {
-    
-    const colorMode=localStorage.getItem(storageKey);
-    root.classList.add(colorMode || defaultMode);
-    updateToggleButton();
+  const colorMode = localStorage.getItem(storageKey);
+  root.classList.add(colorMode || defaultMode);
+  updateToggleButton();
 }
 
 loadColorMode();
 
-// Toggle the color mode 
-toggleButton.addEventListener("click",()=>{
-
-    saveColorMode();
+// Toggle the color mode
+toggleButton.addEventListener("click", () => {
+  saveColorMode();
 });
 
-
-//Saving the user's preferred color mode to local storage 
+// Save the users's preffered color mode to local storage
 function saveColorMode() {
-    //Check if the root element has a class of dark-mode. if yes then the currewnt 
-    // mode is switched to light and vice versa 
-
-    const currentMode = root.classList.contains("dark-mode")
-     ? "light-mode"
-     :"dark-mode";
-    root.classList.remove("light-mode","dark-mode");
-    root.classList.add(currentMode);
-    localStorage.setItem(storageKey,currentMode);
-    updateToggleButton();
-
-
+  // Check if the root element has a class of "dark-mode". If yes, then the current mode is switched to light, and vice versa.
+  const currentMode = root.classList.contains("dark-mode")
+    ? "light-mode"
+    : "dark-mode";
+  root.classList.remove("light-mode", "dark-mode");
+  root.classList.add(currentMode);
+  localStorage.setItem(storageKey, currentMode);
+  updateToggleButton();
 }
-
 
 function updateToggleButton() {
-    if(root.classList.contains("dark-mode")) {
-        toggleButton.style.backgroundImage = "var(--moon)";
-    } else 
-    {
-        toggleButton.style.backgroundImage = "var(--sun)";
-    }
+  if (root.classList.contains("dark-mode")) {
+    toggleButton.style.backgroundImage = "var(--moon)";
+  } else {
+    toggleButton.style.backgroundImage = "var(--sun)";
+  }
 }
-
